5be7ced21892f9c63eeb45bcae22728d82e8a75c
eichmann <david-eichmann@uiowa.edu>
2021-03-12 22:40:44 +0000

    Configuring harvester for the Achilles data.

---
+++ src/main/java/org/cd2h/n3c/Foundry/AchillesDataFetch.java
@@ -0,0 +1,34 @@
+package org.cd2h.n3c.Foundry;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.cd2h.n3c.Foundry.util.APIRequest;
+import org.cd2h.n3c.Foundry.util.PropertyLoader;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class AchillesDataFetch extends CohortDataFetch {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+		PropertyConfigurator.configure(args[0]);
+		prop_file = PropertyLoader.loadProperties("n3c_foundry");
+		conn = APIRequest.getConnection(prop_file);
+		conn.setSchema("enclave_data");
+		initializeReserveHash();
+		
+		JSONObject result = APIRequest.fetchDirectory(prop_file.getProperty("achilles.directory"));
+		JSONArray  array  = result.getJSONArray("values");
+		for (int  i = 0; i < array.length();  i++)  {
+			JSONObject element  = array.getJSONObject(i);
+			String name  = element.getString("name");
+			logger.info("name: " +  name);
+			String rid  = element.getString("rid");
+			logger.info("\trid:  " +  rid);
+			process(name, rid);
+		}
+
+		conn.close();
+	}
+}
