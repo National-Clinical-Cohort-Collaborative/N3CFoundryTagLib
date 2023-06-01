8f6269a8f669ca89d8de6bcdce8532a3f1758bad
eichmann <david-eichmann@uiowa.edu>
2023-06-01 16:29:02 +0000

    Adding another AWS foreign schema to neuromancer.

--- src/non-packaged-resources/aws_configuration.sql
+++ src/non-packaged-resources/aws_configuration.sql
@@ -3,6 +3,8 @@ create server aws foreign data wrapper postgres_fdw options(host 'drupal-prod-rd
 create user mapping for eichmann server aws options(user 'drupal',password '');
 create schema aws_n3c_admin;
 import foreign schema n3c_admin from server aws into aws_n3c_admin;
+create schema aws_enclave_cohort;
+import foreign schema enclave_cohort from server aws into aws_enclave_cohort;
 
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 create server deep_thought foreign data wrapper postgres_fdw options(host 'deep-thought.info-science.uiowa.edu',port '5432',dbname 'loki');
