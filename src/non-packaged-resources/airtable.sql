f7f7a711bc202f14958f71015bbb9006624d0d68
eichmann <david-eichmann@uiowa.edu>
2024-05-16 18:49:25 +0000

    Adding support for airtable dua management.

--- src/non-packaged-resources/airtable.sql
+++ src/non-packaged-resources/airtable.sql
@@ -29,4 +29,86 @@ select
 	(raw->'fields'->>'lastNameMinor')::text as last_name_minor,
 	(raw->'fields'->>'emailMinor')::text as email_minor,
 	(raw->'fields'->>'notes')::text as notes
-from airtable.citizens_raw;	
+from airtable.citizens_raw;
+
+create view airtable.contact as
+select
+	(raw->>'id')::text as id,
+	(raw->'fields'->>'firstName')::text as first_name,
+	(raw->'fields'->>'lastName')::text as last_name,
+	(raw->'fields'->>'contactName')::text as contact_name,
+	(raw->'fields'->>'contactEmail')::text as contact_email,
+	jsonb_array_elements_text(raw->'fields'->'institutionID') as inst_id
+from airtable.contacts_raw;
+
+create view airtable.role as
+select
+	(raw->>'id')::text as id,
+	(raw->'fields'->>'roleType')::text as role
+from airtable.roles_raw;
+
+create or replace view airtable.tenant_dua_master_staging as
+select
+	(raw->>'id')::text as id,
+	raw->'fields'->'inst'->>0 as inst,
+	raw->'fields'->'institutionID'->>0 as inst_id,
+	(raw->'fields'->>'Institution Name')::text as inst_name,
+	raw->'fields'->'clinicalDuaContacts'->>0 as contacts,
+	raw->'fields'->'clinicalDuaSignatory'->>0 as signatory,
+	(raw->'fields'->>'clinicalDuaExecuted')::date as executed
+from airtable.agreements_raw
+where raw->'fields'->>'clinicalDuaExecuted'::text is not null;
+
+create or replace view airtable.tenant_dua_master as
+select
+	dms.inst_id as institutionid,
+	inst_name as institutionname,
+	foo.first_name as tenantduacontactfirst,
+	foo.last_name as tenantduacontactlast,
+	null as tenantduacontactrole,
+	foo.contact_email as tenantduacontactemail,
+	executed as tenantduaexecuted,
+	bar.first_name as tenantinstitutionalsigneefirst,
+	bar.last_name as tenantinstitutionalsigneelast,
+	null as tenantinstitutionalsigneerole,
+	bar.contact_email as tenantinstitutionalsigneeemail
+from tenant_dua_master_staging as dms
+left outer join contact as foo
+on contacts = foo.id
+left outer join contact as bar
+on signatory = bar.id
+;
+
+create or replace view airtable.covid_dua_master_staging as
+select
+	(raw->>'id')::text as id,
+	raw->'fields'->'inst'->>0 as inst,
+	raw->'fields'->'institutionID'->>0 as inst_id,
+	(raw->'fields'->>'Institution Name')::text as inst_name,
+	raw->'fields'->'covidDuaContacts'->>0 as contacts,
+	raw->'fields'->'covidDuaSignatory'->>0 as signatory,
+	raw->'fields'->'covidDurContact'->>0 as dur_contact,
+	(raw->'fields'->>'covidDuaDateExecuted')::date as executed
+from airtable.agreements_raw
+where raw->'fields'->>'covidDuaDateExecuted'::text is not null;
+
+create or replace view airtable.covid_dua_master as
+select distinct
+	dms.inst_id as institutionid,
+	inst_name as institutionname,
+	executed as duaexecuted,
+	foo.first_name as duacontactfirstname,
+	foo.last_name as duacontactsurname,
+	null as duacontactrole,
+	foo.contact_email as duacontactemail,
+	bar.first_name as signatoryfirst,
+	bar.last_name as signatorylast,
+	null as signatoryrole,
+	bar.contact_email as signatoryemail,
+	dur_contact as durcontact
+from covid_dua_master_staging as dms
+left outer join contact as foo
+on contacts = foo.id
+left outer join contact as bar
+on signatory = bar.id
+;
